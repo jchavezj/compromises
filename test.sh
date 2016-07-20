e6867b39aede0138430802eed6c0197246a85913
root <root@mundochavez.org>
2016-07-20 22:26:15 +0000

    creating script

---
+++ test.sh
@@ -0,0 +1,61 @@
+#/bin/bash
+# This script locates possible code exploits
+# in php. Not all files detected
+# are compromised
+#
+# Author: Johnny Chavez
+# Version: 0.1.0
+# date 20July2016
+# colors to be used as a reference:
+
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+# Defining the functions that will be used:
+
+function write_header(){
+	local h="$@"
+	echo ${red}"*************************************************************************"${end}
+	echo "     ${h}"
+	echo ${red}"*************************************************************************"${end}
+}
+
+# searches for exploit scan 
+function xpscan {
+	write_header "Now scanning /var/www/ " 
+	find ./ \( -regex '.*\.php$' -o -regex '.*\.cgi$' -o -regex '.*\.inc$' \) -print0 | xargs -0 egrep -il "$PATTERN" | sort
+}
+
+# searching for worpress versions
+
+function wp {
+	write_header "Now checking for wordpress versions"
+	find /var/www/ -name 'version.php' -path '*wp-includes/*' -print -exec grep '$wp_version =' {} \; -exec echo '' \;
+}
+
+function process {
+	write_header "Need to review these log files if they  have data from log file"
+       for i in `locate access_log` ; do echo $i ; egrep -i '(chr\(|system\()|(wget|curl|perl|gcc|chmod|post)%20' $i ; done
+}
+
+
+# This is the pattern to look for
+PATTERN='base64_decode|r0nin|m0rtix|upl0ad|r57shell|cFaTaLisTiCz_Fx|Tukulesto|99shell|shellbot|phpshell|void\.ru|phpremoteview|directmail|bash_history|\.ru/|brute*force|multiviews|cwings|vandal|bitchx|eggdrop|guardservices|psybnc|dalnet|undernet|vulnscan|spymeta|raslan58|Webshell'
+
+# searches various sripts for possible exploints
+printf "%s\n" ${mag}"**************** Beginning of  Scan  ****************"${end} ""
+cd /var/www/
+printf "%s\n" ${yel}"These are the files that match the pattern being searched"${end}
+printf "%s\n" ${yel}"Have your developer verify these scripts before deleting or modifying"${end} ""
+
+# Now calling the functions to work their foo.
+  xpscan
+  wp
+ process
+
+printf "%s\n" ${mag}"**************** Scan complete ****************"${end}
